b4d69dd8b193cb595a826d569e77ca51d4c58ff2
Navarrete360 <jnavarreter2@upao.edu.pe>
2026-05-16 20:45:21 -0500

    Parte 2

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -11,8 +11,21 @@ contract Almacen235439 {
 
     Producto[] public productos;
     
+    address public dirContrato = 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8;
+    
     constructor() {
         console.log("Ejecutado por: 235439 - Jorge Jefferson Navarrete Rivera");
+    }
+
+    // 1. Agregando métodos:
+    // a)Agregar el método agregarElemento(…) public
+    function agregarElemento(uint256 _id, string memory _nombre, uint256 _cantidad) public {
+        productos.push(Producto(_id, _nombre, _cantidad));
+    }
+
+    // b)Agregar el método contarElementos public
+    function contarElementos() public view returns (uint256) {
+        return productos.length;
     }
 
 }
